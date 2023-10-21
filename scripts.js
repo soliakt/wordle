507304c99cb2a790ebdab5d6be14152da6d317b5
Victor Gimeno <soliakt4@gmail.com>
2023-10-21 13:21:17 +0200

    words table and keyboard created

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-
 window.onload = function () {
     function generateLettersSquares(){
-        // Definimos debajo de qué elemento vamos a generar la tabla (parentElement)
-        parentElement = document.getElementById('lettersSquares');
+        // Definimos debajo de qué elemento vamos a generar la tabla (letterSquaresContainer)
+        letterSquaresContainer = document.getElementById('wordsSquares');
 
         const table = document.createElement('table');
         table.className = 'table-words'; // Asignamos la clase 'table-words' a la tabla para darle estilo en css
@@ -18,10 +17,33 @@ window.onload = function () {
             table.appendChild(row);
         }
         // Añadimos la tabla al elemento padre
-        parentElement.appendChild(table);
+        letterSquaresContainer.appendChild(table);
     }
     generateLettersSquares();
     function generateKeyboard(){
         // Definimos debajo de qué elemento vamos a generar la tabla (parentElement)
+        parentElement = document.getElementById('keyboardSquares');
+        const table = document.createElement('table');
+        table.className = 'table-keyboard';
+        const numRows = 3;
+        const numColumns = 10;
+        let lettersCounter = 0;
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        for (let i = 0; i < numRows; i++) {
+            const row = document.createElement('tr');
+            row.classList.add('lettersKeyboard');
+            for (let j = 0; j < numColumns; j++) {
+                const letter = alphabet.charAt(lettersCounter);
+                const cell = document.createElement('td');
+                cell.innerHTML = `<button type="button" id="${letter.toLowerCase()}">${letter}</button>`;
+                row.appendChild(cell);
+                lettersCounter++;
+            }
+            table.appendChild(row);
+        }
+
+        // Añadir la tabla al elemento padre
+        parentElement.appendChild(table);
     }
+    generateKeyboard();
 }

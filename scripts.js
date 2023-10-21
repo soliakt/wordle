ca22252203a2acdb8f8ee346b164beb607b28909
Victor Gimeno <soliakt4@gmail.com>
2023-10-21 12:46:29 +0200

    initial repository

---
+++ scripts.js
@@ -0,0 +1,31 @@
+/*
+    Alumno: Víctor Gimeno Sanz
+    NIA: 10017766
+    Práctica: Wordle
+*/
+window.onload = function () {
+    function generateLettersSquares(){
+        // Definimos debajo de qué elemento vamos a generar la tabla (parentElement)
+        parentElement = document.getElementById('lettersSquares');
+
+        const table = document.createElement('table');
+        table.className = 'table-words'; // Asignamos la clase 'table-words' a la tabla para darle estilo en css
+        const numRows = 6;
+        const numColumns = 5;
+
+        for (let i = 0; i < numRows; i++) {
+            const row = document.createElement('tr');
+            for (let j = 0; j < numColumns; j++) {
+                const cell = document.createElement('td');
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
+        // Añadimos la tabla al elemento padre
+        parentElement.appendChild(table);
+    }
+    generateLettersSquares();
+    function generateKeyboard(){
+        // Definimos debajo de qué elemento vamos a generar la tabla (parentElement)
+    }
+}
